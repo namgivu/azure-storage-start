5714da0d2a89ca0dda493eae54c909c987273bb9
namgivu <namgivu@gmail.com>
2020-04-21 11:37:06 +0700

    merge of "master" and "origin/trang"

--- tests/test_azure_storage_blob/test_demo.py
+++ tests/test_azure_storage_blob/test_demo.py
@@ -10,21 +10,21 @@ load_dotenv()  # load the .env for unittest; required in macos
 
 class Test:
 
-    def test00__AZURE_STORAGE_CONNECTION_STRING(self):
+    def test00_AZURE_STORAGE_CONNECTION_STRING(self):
         """
         in .env require AZURE_STORAGE_CONNECTION_STRING
         """
         connect_str = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
         assert connect_str is not None
 
-    def test01__open_connection(self):
+    def test01_open_connection(self):
         """
         sample code from quickstart guide
         ref. https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-python
         ref. https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-python#get-the-connection-string
         """
         connect_str = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
-        blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+        _           = BlobServiceClient.from_connection_string(connect_str)
 
     def test02_create_container(self):
         """
@@ -32,20 +32,16 @@ class Test:
         ref. https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-python
         ref. https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-python#get-the-connection-string
         """
-        connect_str = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
+        connect_str         = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
         blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-
         container_name = f"test-{datetime.now().strftime('%Y%m%d-%H%M%S')}"
-
-        container_client = blob_service_client.create_container(container_name)
+        _                   = blob_service_client.create_container(container_name)
 
     def test03_upload_dummy_pdf(self):
-        connect_str = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
+        connect_str         = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
         blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-
-        container_name = f"test{datetime.now().strftime('%Y%m%d-%H%M%S')}"
-
-        container_client = blob_service_client.create_container(container_name)
+        container_name      = f"test{datetime.now().strftime('%Y%m%d-%H%M%S')}"
+        container_client    = blob_service_client.create_container(container_name)
 
         blob_client = blob_service_client.get_blob_client(container=container_name, blob=f"dummy-{datetime.now().strftime('%Y%m%d-%H%M%S')}")
         with open(f'{path}/dummy.pdf', "rb") as data:
@@ -73,7 +69,7 @@ class Test:
             download_file.write(blob_client.download_blob().readall())
 
     def test05_delete_container(self):
-        connect_str = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
+        connect_str         = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
         blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
         container_name = f"test-{datetime.now().strftime('%Y%m%d-%H%M%S')}"
