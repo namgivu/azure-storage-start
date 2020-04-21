0f70147d69162f87011feeeec579eb7ee0bbb4a5
namgivu <namgivu@gmail.com>
2020-04-21 23:53:03 +0700

    review run test

--- bin/run-test.sh
+++ bin/run-test.sh
@@ -2,20 +2,13 @@
 docstring_details=cat<<EOF
        ./bin/run-test.sh       # run all testcases
 skip=1 ./bin/run-test.sh       # skip pipenv sync ie reuse current .venv/
-       ./bin/run-test.sh -n 2  # parallel run in 2 threads
+       ./bin/run-test.sh -n 2  # run in parallel of 2 threads if cpu supported; otherwise will be 1
+
+skip=1 ./bin/run-test.sh -s tests/test_azure_storage_blob/test_quickstart.py::Test::test01_open_connection
+#                        #run specific testcase        at this file           class test_method
 EOF
 
-docstring_details=cat<<EOF
-note='can replace "pipenv run pytest" by ./bin/run-test.sh'
+SH=$(cd `dirname $BASH_SOURCE` && pwd)  # SH aka SCRIPT_HOME for xx ie executed script's containing folder
 
-pipenv run pytest  # run all testcases
-
-pipenv run pytest -n 2
-# run in parallel of 2 threads if cpu supported; otherwise will be 1
-
-pipenv run pytest -s tests/test_azure_storage_blob/test_quickstart.py::Test::test03_upload_dummy_pdf
-#                 #run specific testcase        at this file           class test_method
-EOF
-
-[[ -z $skip ]] || pipenv sync;        pipenv run pytest $@
-# ensure package installed     # run test        any extra params applied
+[[ -z $skip ]] && pipenv sync; PYTHONPATH="$SH/.." pipenv run pytest $@
+# ensure package installed                        # run test        any extra params applied
