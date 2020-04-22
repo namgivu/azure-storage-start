53a1e74b629adf7a0e9eb6dfdd55c0945b437912
namgivu <namgivu@gmail.com>
2020-04-22 20:34:19 +0700

    reader friendly

--- tests/test_azure_storage/test_file_share.py
+++ tests/test_azure_storage/test_file_share.py
@@ -14,10 +14,10 @@ class Test:
         connect_str   = os.environ.get('AZURE_STORAGE_CONNECTION_STRING')
         folder_client = ShareDirectoryClient.from_connection_string(conn_str=connect_str, share_name=EXISTING_SHARE_NAME, directory_path='')
 
-        my_list = list(folder_client.list_directories_and_files())
-        for f in my_list:
+        f_all = list(folder_client.list_directories_and_files())
+        for f in f_all:
             assert isinstance(f, dict)
             k='name'         ; v=f.get(k); assert v is not None; assert isinstance(v, str)
             k='is_directory' ; v=f.get(k); assert v is not None; assert isinstance(v, bool)
 
-        print(f'\n{my_list}')
+        print(f'\n{f_all}')
